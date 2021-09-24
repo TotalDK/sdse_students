56e4405dadf6374674ff02716362521b88062fbf
TotalDK <simon009@breum.dk>
2021-09-24 09:32:38 +0200

    Added data class for city records

--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
@@ -26,6 +26,8 @@ public class CityCSVProcessor {
 				System.out.println("id: " + id + ", year: " + year + ", city: " + city + ", population: " + population);
 				
 				//TODO: Extend the program to process entries!
+				CityRecord record = new CityRecord(id, year, city, population);
+				System.out.println(record);
 			}
 		} catch (Exception e) {
 			System.err.println("An error occurred:");
